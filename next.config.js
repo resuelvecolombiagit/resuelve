d621c4663c26dfabbbde7384411d5d2740988321
sirzes02 <sirzes02@gmail.com>
2023-09-06 18:37:59 -0500

    text images loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
 };
 
 module.exports = nextConfig;
