4332308aaa182dbb9ef794fdde33984af85cb2b7
sirzes02 <sirzes02@gmail.com>
2023-09-06 18:54:17 -0500

    fix load images 3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/resuelvecolombiagit",
+  basePath: "/resuelve",
 };
 
 module.exports = nextConfig;
