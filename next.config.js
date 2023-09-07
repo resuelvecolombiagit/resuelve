701a4471945633d774285fe1cfa1f11cf355800a
sirzes02 <sirzes02@gmail.com>
2023-09-06 19:40:00 -0500

    TEST

--- next.config.js
+++ next.config.js
@@ -3,8 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: "imgix",
-    path: "https://resuelvecolombiagit.github.io/resuelve/",
+    path: "https://resuelvecolombiagit.github.io/",
   },
+  basePath: "/resuelve",
+  assetPrefix: "/resuelve",
 };
 
 module.exports = nextConfig;
