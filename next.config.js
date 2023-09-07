79d3f1b7f582ff54e90461e170e218ae9f58bcad
sirzes02 <sirzes02@gmail.com>
2023-09-06 19:52:22 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    loader: "imgix",
-    path: "https://resuelvecolombiagit.github.io/",
+    //loader: "imgix",
+    //path: "https://SodiaOff.github.io/",
   },
-  basePath: "/resuelve",
-  assetPrefix: "/resuelve",
+  webpack(config, options) {
+    const { isServer } = options;
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      exclude: config.exclude,
+      use: [
+        {
+          loader: require.resolve("url-loader"),
+          options: {
+            limit: config.inlineImageLimit,
+            fallback: require.resolve("file-loader"),
+            publicPath: `${config.assetPrefix}/_next/static/images/`,
+            outputPath: `${isServer ? "../" : ""}static/images/`,
+            name: "[name]-[hash].[ext]",
+            esModule: config.esModule || false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
