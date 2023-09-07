45fd740ad99ed0a4775760a0b602a53da4ee60dc
sirzes02 <sirzes02@gmail.com>
2023-09-06 19:00:35 -0500

    text iamges loader 4

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/resuelve",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
